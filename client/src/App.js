c99ace1906aecece40332b885509b3650ddbb4f7
Alex Chui <ahhchui@gmail.com>
2020-08-12 15:42:12 -0700

    added first fe

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as axios from "axios";
+
+import {Upload, message, Tooltip, Button} from "antd";
+import {UploadOutlined} from "@ant-design/icons";
 
 function App() {
+
+  const [classification, setClassification] = React.useState();
+  const [url, setUrl] = React.useState();
+
+  const handleUrlRequest = (e) => {
+    axios.post("/image-from-url", {url: url}).then(() => {
+      setClassification(response.data.classification);
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {classification ? <div>
+          <div>
+            I'm {classification[0].probability * 100}% sure that this is:
+          </div>
+          <div style={{fontWeigt: "bold", fontSize: "2em"}}>
+            {classification[0].className}
+          </div>
+          {classification[1] ? <span>
+            <div>
+              Although it may also be...
+            </div>
+            <div>
+              {classification[1].className}
+            </div>
+          </span> : null}
+        </div> : 
+        <div>
+          <Upload
+            name={"upload"}
+            showUploadList={false}
+            action={"/image"}
+            onChange={info => {
+              if (info.file.status === "done") {
+                setClassification(info.file.response.classification);
+              } else if (info.file.status === "error") {
+                message.error(infot.file.response);
+              }
+            }}
+          >
+            <Tooltip title={"Upload an image"}>
+              <Button icon={<UploadOutlined />} />
+            </Tooltip>
+          </Upload>
+          <div style={{textAlign: "center"}}>
+            or
+          </div>
+          <Input onChange={e => setUrl(e.target.value)} placeholder={"Or paste a link..."} />
+          <Button onClick={() => handleUrlRequest()}>Go</Button>
+      </div>
+    }
+  </div>
   );
 }
 
